d1e4fafadbbf2a31745aac0779931e5c02d0261d
Abijith CG <abijithc140@gmail.com>
2026-05-21 02:32:26 +0530

    Add EvenOddif program to check if a number is even or odd

---
+++ EvenOddif.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class EvenOddif{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+         System.out.println("Enter an number for check Even or Odd: ");
+          int a = input.nextInt();
+           if(a%2==0){
+            System.out.println("The number " + a + " is Even ");
+           }else{
+            System.out.println("The number " + a + " is Odd ");
+           }
+    }
+}
