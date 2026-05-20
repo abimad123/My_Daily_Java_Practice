3d9f02834cf5ad89139edf90c9ffa1ca4badfcaa
Abijith CG <abijithc140@gmail.com>
2026-05-21 02:13:31 +0530

    USER INPUT IN JAVA

---
+++ InputExample.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class InputExample{
+public static void main(String args[]){
+    Scanner input =new Scanner(System.in);
+      System.out.println("Enter an Number: ");
+       int num=input.nextInt();
+        System.out.println("Number is: "+num);
+    }
+}
