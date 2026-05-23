b0efe0cda58028a5b7ffec1cc30b685e76d99111
Abijith CG <abijithc140@gmail.com>
2026-05-24 03:50:04 +0530

    array with input

--- Arrays/Arrayexample.java
+++ Arrays/Arrayexample.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
+// class Arrayexample{
+//     public static void main(String args[]){
+//     int arr[]={10,20,30,40};
+//       for(int i=0;i<arr.length;i++){          // arr.length - Gives total size of array.
+//         System.out.println(arr[i]);
+//       }
+
+//     }
+// }
+
 class Arrayexample{
     public static void main(String args[]){
-    int arr[]={10,20,30,40};
-      for(int i=0;i<arr.length;i++){          // arr.length - Gives total size of array.
-        System.out.println(arr[i]);
-      }
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter array size:- ");
+        int n=input.nextInt();
 
+        int arr[] = new int[n];  // Create the array with the size 'n' BEFORE the loop
+
+       for(int i=0;i<n;i++){          
+          System.out.println("Enter array:- ");
+          arr[i]=input.nextInt();  // Assign input to a specific index
+        } 
+         System.out.println("Array is:- ");  
+          for(int i=0;i<arr.length;i++){     
+            System.out.println(arr[i]);
+           }
+ 
     }
 }
