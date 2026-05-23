212ee6a0848ecce488a66fb8fc0f5fd16c5c9974
Abijith CG <abijithc140@gmail.com>
2026-05-24 03:43:03 +0530

    array started

---
+++ Arrays/Arrayexample.java
@@ -0,0 +1,9 @@
+class Arrayexample{
+    public static void main(String args[]){
+    int arr[]={10,20,30,40};
+      for(int i=0;i<arr.length;i++){          // arr.length - Gives total size of array.
+        System.out.println(arr[i]);
+      }
+
+    }
+}
