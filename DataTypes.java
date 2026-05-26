22fc9898aaea829c64d006a8c5ac674ea2cfb0e8
Abijith CG <abijithc140@gmail.com>
2026-05-26 16:32:45 +0530

    nothing

--- DataTypes.java
+++ DataTypes.java
@@ -15,3 +15,19 @@ class Datatypes{
         System.out.println(right);
     }
 }
+
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        boolean[] s = new boolean['z' + 1];
+        for (int i = 0; i < word.length(); ++i) {
+            s[word.charAt(i)] = true;
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (s['a' + i] && s['A' + i]) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
